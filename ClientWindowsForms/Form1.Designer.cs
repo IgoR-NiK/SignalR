0ab16ff1cc7c31872db65022fda68fb9cf57b7fb
Igor Nikolaev <Igor_2501@mail.ru>
2017-11-23 02:03:02 +0300

    Добавлена реализация клиента на windows Forms

--- ClientWindowsForms/Form1.Designer.cs
+++ ClientWindowsForms/Form1.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSendMessage = new System.Windows.Forms.Button();
+            this.txtMessages = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSendMessage
+            // 
+            this.btnSendMessage.Location = new System.Drawing.Point(197, 10);
+            this.btnSendMessage.Name = "btnSendMessage";
+            this.btnSendMessage.Size = new System.Drawing.Size(75, 23);
+            this.btnSendMessage.TabIndex = 0;
+            this.btnSendMessage.Text = "Отправить";
+            this.btnSendMessage.UseVisualStyleBackColor = true;
+            this.btnSendMessage.Click += new System.EventHandler(this.btnSendMessage_Click);
+            // 
+            // txtMessages
+            // 
+            this.txtMessages.Location = new System.Drawing.Point(12, 76);
+            this.txtMessages.Multiline = true;
+            this.txtMessages.Name = "txtMessages";
+            this.txtMessages.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtMessages.Size = new System.Drawing.Size(260, 173);
+            this.txtMessages.TabIndex = 1;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(12, 12);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(179, 20);
+            this.txtMessage.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Сообщения";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtMessages);
+            this.Controls.Add(this.btnSendMessage);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSendMessage;
+        private System.Windows.Forms.TextBox txtMessages;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ClientWindowsForms/Form1.cs
+++ ClientWindowsForms/Form1.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,30 @@ namespace ClientWindowsForms
 {
     public partial class Form1 : Form
     {
+        HubConnection hubConnection;
+        IHubProxy hubProxy;
+        SynchronizationContext context;
+
         public Form1()
         {
             InitializeComponent();
+
+            hubConnection = new HubConnection("http://localhost:51188/signalr");
+            hubProxy = hubConnection.CreateHubProxy("MyHub");
+            context = SynchronizationContext.Current;
+
+            hubProxy.On<string>("sendMessageClient", (message) =>
+              context.Post(delegate
+              {
+                  txtMessages.Text += message + Environment.NewLine;
+              }, null));
+            hubConnection.Start();
+        }
+
+        private void btnSendMessage_Click(object sender, EventArgs e)
+        {
+            hubProxy.Invoke("SendMessageServer", txtMessage.Text);
+            txtMessage.Clear();
         }
     }
 }
