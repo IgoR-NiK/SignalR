2c1cb8e1242b8f169a98cfb46294acebc6d3b6a9
IgoR-NiK <Igor_2501@mail.ru>
2019-01-27 22:46:05 +0300

    В XamarinForms для чата создана VM

---
+++ ClientXamarinForms/ClientXamarinForms/ViewModels/ChatVM.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ClientXamarinForms.ViewModels
+{
+	class ChatVM : INotifyPropertyChanged
+	{
+		HubConnection hubConnection;
+		IHubProxy hubProxy;
+
+		public string UserName { get; set; }
+
+		private string message;
+		public string Message
+		{
+			get => message;
+			set
+			{
+				message = value;
+				OnPropertyChanged("Message");
+			}
+		}
+
+		public ObservableCollection<MessageData> Messages { get; } = new ObservableCollection<MessageData>();
+
+		public Command SendComman { get; }
+
+		public ChatVM()
+		{
+			hubConnection = new HubConnection("http://www.signalrtest.somee.com/signalr");
+			hubProxy = hubConnection.CreateHubProxy("MyHub");
+
+			hubProxy.On<string, string>("sendMessageClient", (name, message) => Messages.Add(new MessageData() { User = name, Message = message }));
+			hubConnection.Start();
+
+			SendComman = new Command(() =>
+			{
+				if (hubConnection.State == ConnectionState.Connected)
+				{
+					hubProxy.Invoke("SendMessageServer", UserName, Message);
+					Message = String.Empty;
+				}
+			});
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void OnPropertyChanged([CallerMemberName]string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+
+	public class MessageData
+	{
+		public string Message { get; set; }
+		public string User { get; set; }
+	}
+}
--- ClientXamarinForms/ClientXamarinForms/Views/ChatPage.xaml.cs
+++ ClientXamarinForms/ClientXamarinForms/Views/ChatPage.xaml.cs
@@ -1,60 +1,17 @@
-﻿using System;
-using Microsoft.AspNet.SignalR.Client;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using System.Collections.ObjectModel;
+using ClientXamarinForms.ViewModels;
 
 namespace ClientXamarinForms.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ChatPage : ContentPage
 	{
-		HubConnection hubConnection;
-		IHubProxy hubProxy;
-
-		public string UserName { get; set; }
-
-		private string message;
-		public string Message
-		{
-			get => message;
-			set
-			{
-				message = value;
-				OnPropertyChanged("Message");
-			}
-		}
-
-		public ObservableCollection<MessageData> Messages { get; } = new ObservableCollection<MessageData>();
-
 		public ChatPage()
 		{
 			InitializeComponent();
 
-			BindingContext = this;
-
-			hubConnection = new HubConnection("http://www.signalrtest.somee.com/signalr");
-			hubProxy = hubConnection.CreateHubProxy("MyHub");
-
-			hubProxy.On<string, string>("sendMessageClient", (name, message) => Messages.Add(new MessageData() { User = name, Message = message }));			
-			hubConnection.Start();
+			BindingContext = new ChatVM();
 		}
-
-		private void BtnSend_Clicked(object sender, EventArgs e)
-		{
-			hubProxy.Invoke("SendMessageServer", UserName, Message);
-			Message = String.Empty;
-		}
-	}
-
-	public class MessageData
-	{
-		public string Message { get; set; }
-		public string User { get; set; }
 	}
 }
